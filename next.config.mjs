33446835d43f2b3171e63bf2f3d3ac74859e4d28
slavkov96 <nikolay.slavkov96@gmail.com>
2024-10-05 13:49:57 +0300

    Add Dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: false,
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
